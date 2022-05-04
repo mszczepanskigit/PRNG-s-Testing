0d86ebc7d89f1d53595c7b4c2f43d888198dc7db
WeronikaSzota <104278364+WeronikaSzota@users.noreply.github.com>
2022-05-04 16:36:39 +0200

    Add files via upload

---
+++ chi2_test_new.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from scipy.stats import chisquare
+from scipy.stats.distributions import chi2
+
+# obs : zaobserwowana liczba kul w i-tym pudelku
+# exp : oczekiwana liczba kul w i-tym pudelku
+
+def chi2_test(obs, exp):
+    chi_square_stat, p_value = chisquare(obs, exp)
+    #print("chi_square_stat = ", chi_square_stat, ", p-value = ", p_value)
+    deg_of_freedom = len(obs) - 1
+    print("p-value = ", chi2.sf(chi_square_stat, deg_of_freedom))
+    return chi2.sf(chi_square_stat, deg_of_freedom)
