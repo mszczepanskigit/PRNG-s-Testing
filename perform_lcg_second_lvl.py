b5e37b2a48b00795ef5b053c7a5e883baf41f86b
mateusz.szczepanski <83230824+mszczepanskigit@users.noreply.github.com>
2022-05-04 23:23:02 +0200

    Add files via upload

---
+++ perform_lcg_second_lvl.py
@@ -0,0 +1,25 @@
+from chi2_new import chi2_test
+from freq_mon_test import freq_mon_test
+from second_level import sec_l_test
+from lcg import lcg
+from int_to_bit import int_to_bit_on_list
+
+if __name__ == "__main__":
+    p_values = []
+    p_values2 = []
+    N = 2**12
+    m2 = lcg(mod=8, a=1, c=1, x_0=1, n=1000 * N)
+    for k in range(1000):
+        m_temp = m2[k * N:(k + 1) * N]
+        print(m_temp)
+        oczekiwane = [N / 8] * 8
+        obserwowane = [0]*8
+
+        for i in m_temp:
+            obserwowane[i] += 1
+        p_values.append(chi2_test(obserwowane, oczekiwane))
+        m_temp = int_to_bit_on_list(m_temp)
+        p_values2.append(freq_mon_test(m_temp)[0])
+
+    sec_l_test(p_values)
+    sec_l_test(p_values2)
